0035e1592d3ad1ebd8777810d8d1c8e5c13f3b86
Jayarch97 <jayquintanilla2@gmail.com>
2025-05-08 01:57:43 +0000

    updating the python target

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
 import os
 
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
-@app.route("/hello", methods=['GET', 'POST'])
+@app.route("/hello", methods=['POST'])
 def recieved_data():
-    if request.method == 'POST':
-        data = request.get_json()  # Get JSON data from the request body
-        if data:
-            title = data.get('title', 'No Title')
-            body = data.get('body', 'No Body')
-            id_value = data.get('id')
-            response_message = f"Data received (POST): Title - {title}, Body - {body}, ID - {id_value}"
-            return jsonify({"message": response_message, "received_data": data}), 200
+        if request.method == 'POST':
+           data = request.get_json() 
+           print(data)
+           return data, 200
         else:
-            return jsonify({"error": "No JSON data received in the request body"}), 400
-    else:  # Handle GET requests
-        return "Hello from the GET request!"
+            return "The post method did not work"
 
 
 if __name__ == "__main__":
